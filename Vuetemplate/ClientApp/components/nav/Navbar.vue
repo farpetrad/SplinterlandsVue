078002fbd9fe4240e4b140c17133b88ecf7d60eb
farpetrad <farpetrad@gmail.com>
2022-02-20 21:48:48 -0800

    profile avatar working, some drop down enhancements

--- Vuetemplate/ClientApp/components/nav/Navbar.vue
+++ Vuetemplate/ClientApp/components/nav/Navbar.vue
@@ -19,13 +19,18 @@
                     </template>
                 </div>
                 <div class="ml-auto">
-                    <div :class="{ 
-                           'dropdown row h-100 pt-4 mx-auto' : true,
-                            'dropright' : mobileMenu
-                         }"
-                         >
-                        <div class="col-12 col-lg-8">
-                            <a class="dropdown-toggle text-uppercase"
+                    <div :class="{
+                           'dropdown row h-100 pt-1 mx-auto px-3  mb-4 mb-lg-0' : true,
+                            'dropright pt-3' : mobileMenu
+                         }">
+                        <div :class="{ 'mr-2': !mobileMenu, 'col-12': mobileMenu }">
+                            <div class="dropdown-profile">
+                                <img src="https://d36mxiodymuqjm.cloudfront.net/website/icons/avatars/avatar-frame_diamond.png"
+                                     class="dropdown-profile-frame" />
+                                <img src="https://d36mxiodymuqjm.cloudfront.net/website/icons/avatars/avatar_12.png"
+                                     class="dropdown-profile-avatar" />
+                            </div>
+                            <a class="dropdown-toggle text-uppercase ml-4 ml-lg-0"
                                ref="profileDropdownTrigger"
                                href="#"
                                role="button"
@@ -41,8 +46,8 @@
                                 <a class="dropdown-item" href="#">Something else here A</a>
                             </div>
                         </div>
-                        <div class="col-12 col-lg-4 pt-2 pt-lg-0">
-                            <a class="dropdown-toggle"
+                        <div :class="{ 'ml-2': !mobileMenu, 'col-12 pt-3': mobileMenu }">
+                            <a class="dropdown-toggle ml-4 ml-lg-0"
                                ref="langDropdownTrigger"
                                href="#"
                                role="button"
@@ -73,14 +78,14 @@
         data() {
             return {
                 buttons: [
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_shop_active@2x.png', id: 'menu_item_shop', href: '', alt: 'Shop', label: 'Shop', show: false, },
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_open_active@2x.png', id: 'menu_item_open', href: '', alt: 'Open', label: 'Open', show: false,},
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_market_active@2x.png', id: 'menu_item_market', href: '', alt: 'Market', label: 'Market', show: false, },
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_cards_active@2x.png', id: 'menu_item_cards', href: '', alt: 'Cards', label: 'Cards', show: false,},
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_battle_active@2x.png', id: 'menu_item_battle', href: '', alt: 'Battle', label: 'Battle!', show: false,},
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_events_active@2x.png', id: 'menu_item_events', href: '', alt: 'Events', label: 'Events', show: false,},
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_guilds_active@2x.png', id: 'menu_item_guilds', href: '', alt: 'Guilds', label: 'Guilds', show: false,},
-                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_faq_active@2x.png', id: 'menu_item_help', href: '', alt: 'Help', label: 'Help', show: false, }
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_shop_active@2x.png', id: 'menu_item_shop', href: '', alt: 'Shop', label: 'Shop', },
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_open_active@2x.png', id: 'menu_item_open', href: '', alt: 'Open', label: 'Open',},
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_market_active@2x.png', id: 'menu_item_market', href: '', alt: 'Market', label: 'Market', },
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_cards_active@2x.png', id: 'menu_item_cards', href: '', alt: 'Cards', label: 'Cards',},
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_battle_active@2x.png', id: 'menu_item_battle', href: '', alt: 'Battle', label: 'Battle!',},
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_events_active@2x.png', id: 'menu_item_events', href: '', alt: 'Events', label: 'Events',},
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_guilds_active@2x.png', id: 'menu_item_guilds', href: '', alt: 'Guilds', label: 'Guilds',},
+                    { img: 'https://d36mxiodymuqjm.cloudfront.net/website/nav/icon_nav_faq_active@2x.png', id: 'menu_item_help', href: '', alt: 'Help', label: 'Help',}
                 ],
                 langMenuExpanded: false,
                 profileMenuExpanded: false,
@@ -125,7 +130,7 @@
                 }
             },
             handleResize() {
-                this.mobileMenu = window.matchMedia("screen and (max-width:767px)").matches;
+                this.mobileMenu = window.matchMedia("screen and (max-width:768px)").matches;
             }
         },
         mounted() {
@@ -168,6 +173,31 @@
         }
 
         .dropdown {
+            .dropdown-profile {
+                position: absolute;
+                top: -1vh;
+                left: -3vw;
+
+                .dropdown-profile-frame {
+                    position: absolute;
+                    max-width: 48px;
+                    max-height: 48px;
+                    top: 0;
+                    left: 0;
+                    z-index: 1;
+                }
+
+                .dropdown-profile-avatar {
+                    position: absolute;
+                    max-width: 36px;
+                    max-height: 36px;
+                    z-index: 10;
+                    top: 5px;
+                    left: 24px;
+                    transform: translateX(-50%);
+                }
+            }
+
             &:not(.dropright) {
                 .dropdown-menu-language {
                     left: 4vw;
@@ -179,7 +209,7 @@
 
                 @media (min-width: 992px) {
                     .dropdown-menu-language {
-                        left: -16vw;
+                        left: -5vw;
                     }
 
                     .dropdown-menu-profile {
@@ -189,11 +219,47 @@
 
                 @media (min-width: 1200px) {
                     .dropdown-menu-language {
-                        left: -10vw;
+                        left: -3vw;
                     }
 
                     .dropdown-menu-profile {
                         left: -6vw;
+                    }
+                }
+            }
+
+            &.dropright {
+                .dropdown-menu-language {
+                    left: 6vw;
+                    top: 2.5vh;
+                }
+
+                .dropdown-menu-profile {
+                    top: 0.5vh;
+                    left: 14vw;
+                }
+
+                @media (max-width: 576px) {
+                    .dropdown-menu-language {
+                        left: 10vw;
+                        top: 2.5vh;
+                    }
+
+                    .dropdown-menu-profile {
+                        top: 3.5vh;
+                        left: 18vw;
+                    }
+                }
+
+                @media (min-width: 768px) {
+                    .dropdown-menu-language {
+                        left: 10vw;
+                        top: 5vh;
+                    }
+
+                    .dropdown-menu-profile {
+                        top: 0.5vh;
+                        left: 18vw;
                     }
                 }
             }
